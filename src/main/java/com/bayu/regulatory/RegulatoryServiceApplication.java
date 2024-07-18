e9e7111e4960c4314f15f77849b118fceea1bc74
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-07-18 19:01:11 +0700

    Initial commit

---
+++ src/main/java/com/bayu/regulatory/RegulatoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.regulatory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegulatoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegulatoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bayu/regulatory/RegulatoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayu.regulatory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegulatoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
