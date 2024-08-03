46ca2c67b4010fca50df7e8525f6801353ba76ea
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-03 19:02:45 +0700

    add model SecuritiesISINCode

---
+++ src/main/java/com/bayu/regulatory/model/SecuritiesISINCode.java
@@ -0,0 +1,33 @@
+package com.bayu.regulatory.model;
+
+import com.bayu.regulatory.model.approval.RegulatoryApproval;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Table(name = "regulatory_securities_isin_code")
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SecuritiesISINCode extends RegulatoryApproval {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "external_code_2")
+    private String externalCode2;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "isin_lkpbu")
+    private String isinLKPBU;
+
+    @Column(name = "isin_lbabk")
+    private String isinLBABK;
+
+}
