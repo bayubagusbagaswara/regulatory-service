9fccd0b5dd737c4c1cab57e56931af21f02e81c9
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-04 13:29:52 +0700

    add repository RegulatoryDataChangeRepository

---
+++ src/main/java/com/bayu/regulatory/repository/RegulatoryDataChangeRepository.java
@@ -0,0 +1,25 @@
+package com.bayu.regulatory.repository;
+
+import com.bayu.regulatory.model.RegulatoryDataChange;
+import com.bayu.regulatory.model.enumerator.ApprovalStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RegulatoryDataChangeRepository extends JpaRepository<Long, RegulatoryDataChange> {
+
+    @Query(value = "SELECT CASE WHEN COUNT(*) = :listSize THEN 1 ELSE 0 END " +
+            "FROM regulatory_data_change WHERE id IN :idList", nativeQuery = true)
+    Boolean existsByIdList(@Param("idList") List<Long> idList, @Param("listSize") Integer listSize);
+
+    @Query(value = "SELECT CASE WHEN COUNT(*) = :listSize THEN true ELSE false END " +
+            "FROM RegulatoryDataChange WHERE id IN = :idList AND approvalStatus = :status")
+    Boolean existsByIdListAndStatus(@Param("idList") List<Long> idList,
+                                    @Param("listSize") Long listSize,
+                                    @Param("status") ApprovalStatus status);
+
+}
