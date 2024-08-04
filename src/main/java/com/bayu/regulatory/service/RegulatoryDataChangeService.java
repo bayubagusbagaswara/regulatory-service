5c53e833d5a0a653d240d46654a1ff243b4af5dd
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-04 13:39:50 +0700

    add some method service

--- src/main/java/com/bayu/regulatory/service/RegulatoryDataChangeService.java
+++ src/main/java/com/bayu/regulatory/service/RegulatoryDataChangeService.java
@@ -1,4 +1,41 @@
 package com.bayu.regulatory.service;
 
+import com.bayu.regulatory.dto.RegulatoryDataChangeDTO;
+import com.bayu.regulatory.model.RegulatoryDataChange;
+import com.bayu.regulatory.model.enumerator.ApprovalStatus;
+
+import java.util.List;
+
 public interface RegulatoryDataChangeService {
+
+    RegulatoryDataChange getById(Long id);
+
+    List<RegulatoryDataChange> getAll();
+
+    List<String> findAllMenu();
+
+    String deleteAll();
+
+    void setApprovalStatusIsRejected(RegulatoryDataChangeDTO dataChangeDTO, List<String> errorMessageList);
+
+    void setApprovalStatusIsApproved(RegulatoryDataChangeDTO dataChangeDTO);
+
+    <T> void createChangeActionAdd(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz);
+
+    <T> void createChangeActionEdit(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz);
+
+    <T> void createChangeActionDelete(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz);
+
+    Boolean existByIdListAndStatus(List<Long> idList, Long idListSize, ApprovalStatus approvalStatus);
+
+    boolean existById(Long id);
+
+    List<RegulatoryDataChange> findByMenuAndApprovalStatus(String menu, ApprovalStatus approvalStatus);
+
+    void reject(Long id);
+
+    List<RegulatoryDataChange> getAllByApprovalStatus(String approvalStatus);
+
+    String deleteById(Long id);
+
 }
--- src/main/java/com/bayu/regulatory/service/impl/RegulatoryDataChangeServiceImpl.java
+++ src/main/java/com/bayu/regulatory/service/impl/RegulatoryDataChangeServiceImpl.java
@@ -1,10 +1,15 @@
 package com.bayu.regulatory.service.impl;
 
+import com.bayu.regulatory.dto.RegulatoryDataChangeDTO;
+import com.bayu.regulatory.model.RegulatoryDataChange;
+import com.bayu.regulatory.model.enumerator.ApprovalStatus;
 import com.bayu.regulatory.repository.RegulatoryDataChangeRepository;
 import com.bayu.regulatory.service.RegulatoryDataChangeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -12,5 +17,80 @@ import org.springframework.stereotype.Service;
 public class RegulatoryDataChangeServiceImpl implements RegulatoryDataChangeService {
 
     private final RegulatoryDataChangeRepository regulatoryDataChangeRepository;
+
+    @Override
+    public RegulatoryDataChange getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<RegulatoryDataChange> getAll() {
+        return List.of();
+    }
+
+    @Override
+    public List<String> findAllMenu() {
+        return List.of();
+    }
+
+    @Override
+    public String deleteAll() {
+        return "";
+    }
+
+    @Override
+    public void setApprovalStatusIsRejected(RegulatoryDataChangeDTO dataChangeDTO, List<String> errorMessageList) {
+
+    }
+
+    @Override
+    public void setApprovalStatusIsApproved(RegulatoryDataChangeDTO dataChangeDTO) {
+
+    }
+
+    @Override
+    public <T> void createChangeActionAdd(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz) {
+
+    }
+
+    @Override
+    public <T> void createChangeActionEdit(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz) {
+
+    }
+
+    @Override
+    public <T> void createChangeActionDelete(RegulatoryDataChangeDTO dataChangeDTO, Class<T> clazz) {
+
+    }
+
+    @Override
+    public Boolean existByIdListAndStatus(List<Long> idList, Long idListSize, ApprovalStatus approvalStatus) {
+        return null;
+    }
+
+    @Override
+    public boolean existById(Long id) {
+        return false;
+    }
+
+    @Override
+    public List<RegulatoryDataChange> findByMenuAndApprovalStatus(String menu, ApprovalStatus approvalStatus) {
+        return List.of();
+    }
+
+    @Override
+    public void reject(Long id) {
+
+    }
+
+    @Override
+    public List<RegulatoryDataChange> getAllByApprovalStatus(String approvalStatus) {
+        return List.of();
+    }
+
+    @Override
+    public String deleteById(Long id) {
+        return "";
+    }
 
 }
