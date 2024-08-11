5515a075cdac431ba8eb106d9484d783496ac673
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 11:26:42 +0700

    update query

--- src/main/java/com/bayu/regulatory/repository/SecuritiesISINCodeRepository.java
+++ src/main/java/com/bayu/regulatory/repository/SecuritiesISINCodeRepository.java
@@ -2,8 +2,16 @@ package com.bayu.regulatory.repository;
 
 import com.bayu.regulatory.model.SecuritiesISINCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SecuritiesISINCodeRepository extends JpaRepository<SecuritiesISINCode, Long> {
+
+    @Query(value = "FROM SecuritiesISINCode s WHERE s.externalCode2 = :externalCode")
+    Optional<SecuritiesISINCode> findByExternalCode(@Param("externalCode") String externalCode);
+
 }
