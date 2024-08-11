11e6132803e8bd56d7a7230406bae3146669c9f6
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 16:22:12 +0700

    add method toDTOList

--- src/main/java/com/bayu/regulatory/mapper/RegulatoryDataChangeMapper.java
+++ src/main/java/com/bayu/regulatory/mapper/RegulatoryDataChangeMapper.java
@@ -4,11 +4,15 @@ import com.bayu.regulatory.dto.RegulatoryDataChangeDTO;
 import com.bayu.regulatory.model.RegulatoryDataChange;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface RegulatoryDataChangeMapper {
 
     RegulatoryDataChangeDTO toDTO(RegulatoryDataChange regulatoryDataChange);
 
     RegulatoryDataChange toModel(RegulatoryDataChangeDTO regulatoryDataChangeDTO);
+
+    List<RegulatoryDataChangeDTO> toDTOList(List<RegulatoryDataChange> regulatoryDataChanges);
 
 }
