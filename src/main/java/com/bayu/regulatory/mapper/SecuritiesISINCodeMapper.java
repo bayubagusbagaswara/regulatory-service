3b980a263b1e9300373470fb90c0482c8d6aaf85
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 16:24:00 +0700

    add method mapper

--- src/main/java/com/bayu/regulatory/mapper/SecuritiesISINCodeMapper.java
+++ src/main/java/com/bayu/regulatory/mapper/SecuritiesISINCodeMapper.java
@@ -1,14 +1,32 @@
 package com.bayu.regulatory.mapper;
 
 import com.bayu.regulatory.dto.securitiesisincode.SecuritiesISINCodeDTO;
+import com.bayu.regulatory.dto.securitiesisincode.UploadSecuritiesISINCodeDataRequest;
 import com.bayu.regulatory.model.SecuritiesISINCode;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface SecuritiesISINCodeMapper {
 
+    @Mapping(source = "id", target = "id")
     @Mapping(source = "externalCode2", target = "externalCode")
     SecuritiesISINCodeDTO toDTO(SecuritiesISINCode securitiesISINCode);
 
+    @Mapping(source = "externalCode2", target = "externalCode", qualifiedByName = "nullToEmpty")
+    @Mapping(source = "currency", target = "currency", qualifiedByName = "nullToEmpty")
+    @Mapping(source = "isinLKPBU", target = "isinLKPBU", qualifiedByName = "nullToEmpty")
+    @Mapping(source = "isinLBABK", target = "isinLBABK", qualifiedByName = "nullToEmpty")
+    @Mapping(target = "id", ignore = true) // Ignoring the ID field
+    SecuritiesISINCodeDTO fromUploadRequestToDTO(UploadSecuritiesISINCodeDataRequest dataRequest);
+
+    @Named("nullToEmpty")
+    default String nullToEmpty(String value) {
+        return null == value ? "" : value;
+    }
+
+    List<SecuritiesISINCodeDTO> toDTOList(List<SecuritiesISINCode> all);
 }
