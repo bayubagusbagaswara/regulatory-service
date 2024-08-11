2fa7705846cea8492705a7d17938cf9024b324e3
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 21:16:45 +0700

    add annotation validation

--- src/main/java/com/bayu/regulatory/dto/exchangerate/UpdateExchangeRateRequest.java
+++ src/main/java/com/bayu/regulatory/dto/exchangerate/UpdateExchangeRateRequest.java
@@ -1,6 +1,7 @@
 package com.bayu.regulatory.dto.exchangerate;
 
 import com.bayu.regulatory.dto.approval.InputIdentifierRequest;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 @EqualsAndHashCode(callSuper = true)
@@ -10,13 +11,15 @@ import lombok.*;
 @AllArgsConstructor
 public class UpdateExchangeRateRequest extends InputIdentifierRequest {
 
-    // ID wajib diisi
     private Long id;
 
+    @NotBlank(message = "Currency Code cannot be empty")
     private String code;
 
+    @NotBlank(message = "Currency Name cannot be empty")
     private String name;
 
+    @NotBlank(message = "Rate cannot be empty")
     private String rate;
 
 }
