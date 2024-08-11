81d7a1a48342d43c4232552acf426c9a92ba5616
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 11:26:35 +0700

    update mapper

--- src/main/java/com/bayu/regulatory/mapper/SecuritiesISINCodeMapper.java
+++ src/main/java/com/bayu/regulatory/mapper/SecuritiesISINCodeMapper.java
@@ -3,10 +3,12 @@ package com.bayu.regulatory.mapper;
 import com.bayu.regulatory.dto.securitiesisincode.SecuritiesISINCodeDTO;
 import com.bayu.regulatory.model.SecuritiesISINCode;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface SecuritiesISINCodeMapper {
 
+    @Mapping(source = "externalCode2", target = "externalCode")
     SecuritiesISINCodeDTO toDTO(SecuritiesISINCode securitiesISINCode);
 
 }
