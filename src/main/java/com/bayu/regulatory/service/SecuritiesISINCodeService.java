40e260a792afa20e565ae7c18928450d971f3141
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-08-11 10:42:58 +0700

    add service contract SecuritiesISINCodeService

---
+++ src/main/java/com/bayu/regulatory/service/SecuritiesISINCodeService.java
@@ -0,0 +1,26 @@
+package com.bayu.regulatory.service;
+
+import com.bayu.regulatory.dto.RegulatoryDataChangeDTO;
+import com.bayu.regulatory.dto.securitiesisincode.*;
+
+import java.util.List;
+
+public interface SecuritiesISINCodeService {
+
+    SecuritiesISINCodeResponse uploadData(UploadSecuritiesISINCodeListRequest uploadSecuritiesIssuerCodeListRequest, RegulatoryDataChangeDTO regulatoryDataChangeDTO);
+
+    SecuritiesISINCodeResponse createApprove(ApproveSecuritiesISINCodeRequest approveSecuritiesISINCodeRequest, String approveIPAddress);
+
+    SecuritiesISINCodeResponse updateApprove(ApproveSecuritiesISINCodeRequest approveSecuritiesISINCodeRequest, String approveIPAddress);
+
+    SecuritiesISINCodeResponse deleteById(DeleteSecuritiesISINCodeRequest deleteSecuritiesISINCodeRequest, RegulatoryDataChangeDTO regulatoryDataChangeDTO);
+
+    SecuritiesISINCodeResponse deleteApprove(ApproveSecuritiesISINCodeRequest approveSecuritiesISINCodeRequest, String approveIPAddress);
+
+    SecuritiesISINCodeDTO getById(Long id);
+
+    SecuritiesISINCodeDTO getByExternalCode(String externalCode);
+
+    List<SecuritiesISINCodeDTO> getAll();
+
+}
