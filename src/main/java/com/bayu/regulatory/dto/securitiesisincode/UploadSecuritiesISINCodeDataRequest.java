e9f1ab8d65fe5da323af11a6962f59bb37d6a8f4
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2024-09-28 19:25:11 +0700

    add some validation annotation

--- src/main/java/com/bayu/regulatory/dto/securitiesisincode/UploadSecuritiesISINCodeDataRequest.java
+++ src/main/java/com/bayu/regulatory/dto/securitiesisincode/UploadSecuritiesISINCodeDataRequest.java
@@ -1,7 +1,11 @@
 package com.bayu.regulatory.dto.securitiesisincode;
 
+import com.bayu.regulatory.dto.validation.AddValidationGroup;
+import com.bayu.regulatory.dto.validation.UpdateValidationGroup;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,18 +17,23 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class UploadSecuritiesISINCodeDataRequest {
 
-    @NotBlank(message = "External Code must not be blank")
     @JsonProperty(value = "External Code 2")
+    @NotBlank(message = "External Code must not be blank", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "Code must contain only alphanumeric characters", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private String externalCode2;
 
-    @NotBlank(message = "Currency must not be blank")
     @JsonProperty(value = "Currency")
+    @NotBlank(message = "Currency must not be blank", groups = AddValidationGroup.class)
+    @Pattern(regexp = "^[A-Za-z]+$", message = "Currency must contain only alphabetic characters", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
+    @Size(max = 3, message = "Currency must be at most 3 characters long", groups = {AddValidationGroup.class, UpdateValidationGroup.class})
     private String currency;
 
     @JsonProperty(value = "ISIN LKPBU")
+    @NotBlank(message = "ISIN LKPBU must not be blank", groups = AddValidationGroup.class)
     private String isinLKPBU;
 
     @JsonProperty(value = "ISIN LBABK")
+    @NotBlank(message = "ISIN LBABK must not be blank", groups = AddValidationGroup.class)
     private String isinLBABK;
 
 }
